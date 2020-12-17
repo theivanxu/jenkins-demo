7b52e317ac9400054c92c34dee5b0e71816ec28f
Ivan Xu <theivanxu@gmail.com>
2020-12-17 17:46:55 +0800

    change to http server

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"net/http"
 )
 
-func main()  {
-	fmt.Println("Hello, Kubernetes!I'm from Jenkins CI!")
-	fmt.Println("BRANCH_NAME:", os.Getenv("branch"))
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Ivan")
+}
+
+func main() {
+	fmt.Println("Hello, Kubernetes! I'm from Jenkins CI!")
+	http.HandleFunc("/", HelloHandler)
+	http.ListenAndServe(":8000", nil)
 }
